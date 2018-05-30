03760a81569fffc2f842df68d2a064236e3fc8bd
Shivansh Rajolia <shivansh.rajolia28@gmail.com>
2018-05-31 01:02:08 +0530

    Final Project Commit

---
+++ JS/app.js
@@ -0,0 +1,76 @@
+$(function() {
+    var model = {
+        init: function() {
+            if(!localStorage.hellerNotes) {
+                localStorage.hellerNotes = [];
+            }
+        },
+        addNote: function(noteObj) {
+            var oldData = JSON.parse(localStorage.getItem('hellerNotes'));
+            oldData.push(noteObj);
+            localStorage.setItem('hellerNotes', JSON.stringify(oldData));
+        },
+        getNotes: function() {
+            return JSON.parse(localStorage.hellerNotes);
+        }
+    }
+
+    var octopus = {
+        init: function() {
+            model.init();
+            view.init();
+        },
+        addNewNote: function(newNote) {
+            model.addNote({
+                note: newNote
+            });
+            view.render();
+        },
+        getAllNotes() {
+            return model.getNotes();
+        }
+
+    }
+
+    var view = {
+        init: function() {
+            $('#new-note-content').val('')
+            $('#clear').click(function() {
+                localStorage.hellerNotes = "[]";
+                view.render();
+            });
+            var form = $('#new-note-form');
+            form.submit(function(e) {
+                var newNote = $('#new-note-content').val();
+                if(newNote!== "" && newNote)
+                octopus.addNewNote(newNote);
+                $('#new-note-content').val('')
+                e.preventDefault();
+            });
+            this.render();
+        },
+
+        // clear: function() {
+        //     localStorage.hellerNotes = "[]";
+        // },        
+
+        render: function() {
+            var notesArr = octopus.getAllNotes();
+            // liArr = "";
+            var ul = $('#notes');
+            ul.html('');
+            notesArr.forEach(element => {
+                var li = document.createElement('li');
+                li.setAttribute('class', 'note');
+                var text = document.createTextNode(element.note);
+                li.appendChild(text);
+                ul.append(li);
+                // ul.html(liArr);
+            });
+        }
+
+    }
+
+    octopus.init();
+});
+
